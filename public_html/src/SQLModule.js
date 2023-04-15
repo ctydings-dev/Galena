67455abb21905bb230b746a4932d8045a7526567
ctydi <ctydi@192.168.50.47>
2023-04-14 23:52:08 -0800

    Piped the output to a table format.

--- public_html/src/SQLModule.js
+++ public_html/src/SQLModule.js
@@ -31,11 +31,42 @@ class SQLModule extends BaseModule {
 
         // Bind new values
         stmt.bind();
+
+        var table = null;
+
+        var counter = 0;
+
+
         while (stmt.step()) { //
             const row = stmt.getAsObject();
-            this.getCaller().printText(JSON.stringify(row));
+            if (counter === 0) {
+
+                var cols = [];
+                for (var prop in row) {
+                    cols.push(prop + '');
+
+                }
+
+                table = new TextTable(cols);
+            }
+
+            var index = 0;
+            for (var prop in row) {
+                var value = row[prop];
+                table.setCell(counter, index, value);
+
+                index++;
+            }
+
+
 
         }
+        if (genUtils.isNull(table) !== true) {
+            this.getCaller().printTable(table);
+        }
+
+
+
         this.getCaller().printText('');
 
     }
